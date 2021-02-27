4b81872f1ce2a6e3f8568bdfb46794127429e897
全球变暖 <1445023846@qq.com>
2021-02-27 19:55:25 +0800

    网站首页开发

--- application/home/controller/Web.php
+++ application/home/controller/Web.php
@@ -10,6 +10,18 @@ use library\mysmarty\Controller;
  */
 class Web extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        // 查找所有栏目数据
+        $column = new \application\home\model\Column();
+        $columnData = $column->order('pid asc,id asc')
+            ->field('id,uri,pid,name,type,target_blank,keywords,description')
+            ->neq('status', 3)
+            ->select();
+
+    }
+
     /**
      * 网站首页
      */
