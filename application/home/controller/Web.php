19a4ac61b4171b304262711f8dc616a3ae4f0095
全球变暖 <1445023846@qq.com>
2021-02-28 10:38:16 +0800

    顶部导航

--- application/home/controller/Web.php
+++ application/home/controller/Web.php
@@ -10,16 +10,52 @@ use library\mysmarty\Controller;
  */
 class Web extends Controller
 {
+    // 栏目数据
+    private array $columnData;
+
     public function __construct()
     {
         parent::__construct();
         // 查找所有栏目数据
         $column = new \application\home\model\Column();
         $columnData = $column->order('pid asc,id asc')
-            ->field('id,uri,pid,name,type,target_blank,keywords,description')
+            ->field('id,uri,pid,name,type,target_blank,keywords,description,status')
             ->neq('status', 3)
             ->select();
+        $this->columnData = $columnData;
+        $topColumnData = [];
+        $bottomColumnData = [];
+        foreach ($columnData as $v) {
+            if (1 == $v['status']) {
+                $topColumnData[] = $v;
+            } else if (2 == $v['status']) {
+                $bottomColumnData[] = $v;
+            }
+        }
+        $tmpTopColumnData = [];
+        foreach ($topColumnData as $v) {
+            $tmpTopColumnData[$v['pid']][] = $v;
+        }
+        $this->assign('topColumnData', $this->generateTree($tmpTopColumnData, $tmpTopColumnData[0]));
+        $this->assign('bottomColumnData', $bottomColumnData);
+    }
 
+    /**
+     * 将数组数据转为树形结构
+     * @param array $list 原数组数据
+     * @param array $parent 顶级原数组数据
+     * @return array
+     */
+    private function generateTree(array &$list, array $parent): array
+    {
+        $tree = [];
+        foreach ($parent as $k => $v) {
+            if (isset($list[$v['id']])) {
+                $v['children'] = $this->generateTree($list, $list[$v['id']]);
+            }
+            $tree[] = $v;
+        }
+        return $tree;
     }
 
     /**
